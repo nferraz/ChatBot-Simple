7a27a2c53a14e25966bfd53ec5317541872ec018
Nelson Ferraz <nferraz@gmail.com>
2013-04-26 18:26:42 +0200

    Simple variables in Calculator example

--- examples/Calculator.pm
+++ examples/Calculator.pm
@@ -4,9 +4,20 @@ use ChatBot::Simple;
 
 no warnings 'uninitialized';
 
-pattern qr{(\d+)\s*([\+\-\*\/])\s*(\d+)} => sub {
+my %var;
+
+pattern qr{(\w+)\s*\=\s*(\d+)} => sub {
+    my ($input,$param) = @_;
+    $var{$param->{':1'}} = $param->{':2'};
+    return;
+} => 'ok';
+
+pattern qr{(\d+|\w+)\s*([\+\-\*\/])\s*(\d+|\w+)} => sub {
   my ($input,$param) = @_;
   my ($n1,$op,$n2) = ($param->{':1'},$param->{':2'},$param->{':3'});
+
+  if (exists $var{$n1}) { $n1 = $var{$n1}; }
+  if (exists $var{$n2}) { $n2 = $var{$n2}; }
 
   return
         $op eq '+' ? $n1 + $n2
