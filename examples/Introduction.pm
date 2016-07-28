97f299e2ea9493179ee047ba62ab3a3628811061
Nelson Ferraz <nferraz@gmail.com>
2016-07-28 16:21:46 +0200

    Update example

--- examples/Introduction.pm
+++ examples/Introduction.pm
@@ -4,51 +4,26 @@ use ChatBot::Simple;
 
 no warnings 'uninitialized';
 
-my %mem;
-
 {
     context '';
 
     transform 'hello' => 'hi';
     transform 'goodbye', 'bye-bye', 'sayonara' => 'bye';
 
+    pattern 'hi'  => sub { context 'name' } => "hi! what's your name?";
     pattern 'bye' => 'bye!';
-
-    pattern 'hi' => sub {
-        my ( $input, $param ) = @_;
-        if ( !$mem{name} ) {
-            context 'name';
-            return "hi! what's your name?";
-        }
-    };
 }
 
 {
     context 'name';
-
-    pattern "my name is :name" => sub {
-        my ( $input, $param ) = @_;
-        $mem{name} = $param->{':name'};
-
-        context 'how_are_you';
-        return "Hello, :name! How are you?";
-    };
-
-    pattern qr{^(\w+)$} => sub {
-        my ( $input, $param ) = @_;
-        $mem{name} = $param->{':1'};
-
-        context 'how_are_you';
-        return "Hello, $mem{name}! How are you?";
-      }
+    pattern "my name is :name" => sub { context 'how_are_you' } => "Hello, :name! How are you?";
 }
 
 {
     context 'how_are_you';
 
-    pattern 'fine' => "that's great, :name!";
-
-    pattern qr{^(\w+)$} => 'why do you say that?';
+    pattern 'fine'            => "that's great, :name!";
+    pattern ':something_else' => 'why do you say that?';
 }
 
 1;
